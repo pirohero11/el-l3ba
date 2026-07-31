3e09f3f5d08af5d3564ede239fd0699913c6d5a0
pirohero11 <pirohero11@gmail.com>
2026-07-31 13:42:56 +0300

    Do parent UI

--- src/app/[locale]/parent/homePage/history/page.tsx
+++ src/app/[locale]/parent/homePage/history/page.tsx
@@ -4,31 +4,31 @@ import { getTranslations, setRequestLocale } from "next-intl/server";
 import HistoryContent from "@/components/HistoryContent";
 
 export default async function Events({
-  params,
+    params,
 }: {
-  params: Promise<{ locale: string }>;
+    params: Promise<{ locale: string }>;
 }) {
-  const { locale } = await params;
-  setRequestLocale(locale);
-  const t = await getTranslations("history");
+    const { locale } = await params;
+    setRequestLocale(locale);
+    const t = await getTranslations("history");
 
-  return (
-    <div className="min-h-screen w-screen bg-background flex flex-col items-center overflow-y-auto no-scrollbar">
-        <header className='p-5 w-full flex flex-rew justify-center items-center border-b-black border-2 h-18 sticky top-0 bg-white mb-10'>
-            <TabToggle></TabToggle>
-        </header>
-      {/* Title */}
+    return (
+        <div className="min-h-screen w-screen bg-background flex flex-col items-center overflow-y-auto no-scrollbar">
+            <header className='p-5 w-full flex flex-rew justify-center items-center border-b-black border-2 h-18 sticky top-0 bg-white mb-10'>
+                <TabToggle></TabToggle>
+            </header>
+            {/* Title */}
 
-      {/* Week Accordions */}
-        <HistoryContent
-        translations={{
-            week: t("week"),
-            day: t("day"),
-            taskName: t("taskName"),
-            time: t("time"),
-            status: t("status"),
-        }}
-        />
-    </div>
+            {/* Week Accordions */}
+            <HistoryContent
+                translations={{
+                    week: t("week"),
+                    day: t("day"),
+                    taskName: t("taskName"),
+                    time: t("time"),
+                    status: t("status"),
+                }}
+            />
+        </div>
     );
 }
